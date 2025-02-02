b6745b067ed64929af70256078b24087dbf19327
codify9 <noobcoderhu@gmail.com>
2025-02-02 22:44:35 +0530

    Change the Data set and Drop column where there is null

--- Test1.py
+++ Test1.py
@@ -8,8 +8,10 @@ from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName('Dataframe').getOrCreate()
 
+# Type 1 of reading data
 # df_pyspark = spark.read.option('header', 'true').csv('test.csv', inferSchema = True)
 
+# Type 2 of reading data
 df_pyspark = spark.read.csv('test.csv', header=True, inferSchema=True)
 
 # df_pyspark.show()
@@ -19,14 +21,25 @@ df_pyspark = spark.read.csv('test.csv', header=True, inferSchema=True)
 
 # Adding a New column in Dataframe
 
-df_pyspark.withColumn('Age after 5 Yrs', df_pyspark['age']+5).show()
+# df_pyspark.withColumn('Age after 5 Yrs', df_pyspark['age']+5).show()
 
 # Drop an existing column
 
-df_pyspark.drop('Age after 5 Yrs').show()
+# df_pyspark.drop('Age after 5 Yrs').show()
 
 # Renaming an existing column
 
-df_pyspark.withColumnRenamed('Name', 'Full Name').show()
+# df_pyspark.withColumnRenamed('Name', 'Full Name').show()
 
+# Drop Rows where Null is present
+
+df_pyspark.na.drop().show()
+
+# Drop Rows on How
+
+df_pyspark.na.drop(how="all").show()
+
+# Drop Rows on subset
+
+df_pyspark.na.drop(how="all", subset=['Name']).show()
 

b4a01c623b933f97bae64629b6bd3e57ef1e9aa2
codify9 <noobcoderhu@gmail.com>
2025-02-02 22:13:31 +0530

    Changed the column name

--- Test1.py
+++ Test1.py
@@ -11,9 +11,22 @@ spark = SparkSession.builder.appName('Dataframe').getOrCreate()
 # df_pyspark = spark.read.option('header', 'true').csv('test.csv', inferSchema = True)
 
 df_pyspark = spark.read.csv('test.csv', header=True, inferSchema=True)
-df_pyspark.show()
-df_pyspark.printSchema()
 
+# df_pyspark.show()
+# df_pyspark.printSchema()
 
+# df_pyspark.describe().show()
+
+# Adding a New column in Dataframe
+
+df_pyspark.withColumn('Age after 5 Yrs', df_pyspark['age']+5).show()
+
+# Drop an existing column
+
+df_pyspark.drop('Age after 5 Yrs').show()
+
+# Renaming an existing column
+
+df_pyspark.withColumnRenamed('Name', 'Full Name').show()
 
 

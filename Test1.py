341fd6d7ac0a7e87aad5ca945f482b4f1ec042e3
codify9 <noobcoderhu@gmail.com>
2025-03-10 01:55:11 +0530

    fixed a bug

--- Test1.py
+++ Test1.py
@@ -1,70 +1,73 @@
 import pyspark
 import pandas as pd
 
+
 df = pd.read_csv('test.csv')
+
+# df = pd.read_csv(r"E:\PySpark\test.csv")
 # print(df)
 
 from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName('Dataframe').getOrCreate()
 
-# Type 1 of reading data
-# df_pyspark = spark.read.option('header', 'true').csv('test.csv', inferSchema = True)
+# # Type 1 of reading data
+# # df_pyspark = spark.read.option('header', 'true').csv('test.csv', inferSchema = True)
 
-# Type 2 of reading data
+# # Type 2 of reading data
 df_pyspark = spark.read.csv('test.csv', header=True, inferSchema=True)
 
-# df_pyspark.show()
-# df_pyspark.printSchema()
+df_pyspark.show()
+df_pyspark.printSchema()
 
-# df_pyspark.describe().show()
+# # df_pyspark.describe().show()
 
-# Adding a New column in Dataframe
+# # Adding a New column in Dataframe
 
-# df_pyspark.withColumn('Age after 5 Yrs', df_pyspark['age']+5).show()
+# # df_pyspark.withColumn('Age after 5 Yrs', df_pyspark['age']+5).show()
 
-# Drop an existing column
+# # Drop an existing column
 
-# df_pyspark.drop('Age after 5 Yrs').show()
+# # df_pyspark.drop('Age after 5 Yrs').show()
 
-# Renaming an existing column
+# # Renaming an existing column
 
-# df_pyspark.withColumnRenamed('Name', 'Full Name').show()
+# # df_pyspark.withColumnRenamed('Name', 'Full Name').show()
 
-# Drop Rows where Null is present
+# # Drop Rows where Null is present
 
-# df_pyspark.na.drop().show()
+# # df_pyspark.na.drop().show()
 
-# Drop Rows on How
+# # Drop Rows on How
 
-# df_pyspark.na.drop(how="all").show()
+# # df_pyspark.na.drop(how="all").show()
 
-# Drop Rows on subset
+# # Drop Rows on subset
 
-# df_pyspark.na.drop(how="all", subset=['Name']).show()
+# # df_pyspark.na.drop(how="all", subset=['Name']).show()
 
-# Filling the Null Values.
+# # Filling the Null Values.
 
-# df_pyspark.na.fill('Na', ['Name', 'age']).show()
+# # df_pyspark.na.fill('Na', ['Name', 'age']).show()
 
-# Filling the Null Values by Imputer Function
+# # Filling the Null Values by Imputer Function
 
-# from pyspark.ml.feature import Imputer
+# # from pyspark.ml.feature import Imputer
 
-# imputer = Imputer(
-#     inputCols=['age', 'Experience', 'Salary'],
-#     outputCols=["{}_imputed".format(c) for c in ['age', 'Experience', 'Salary']]  
-# ).setStrategy('mean')
+# # imputer = Imputer(
+# #     inputCols=['age', 'Experience', 'Salary'],
+# #     outputCols=["{}_imputed".format(c) for c in ['age', 'Experience', 'Salary']]  
+# # ).setStrategy('mean')
 
-# imputer.fit(df_pyspark).transform(df_pyspark).show()
+# # imputer.fit(df_pyspark).transform(df_pyspark).show()
 
-# Filtering Data
+# # Filtering Data
 
-# print name of employees whose experience is less then 14.
+# # print name of employees whose experience is less then 14.
 
-df_pyspark.filter('Experience <= 14').select(['Name', 'age']).show()
+# df_pyspark.filter('Experience <= 14').select(['Name', 'age']).show()
 
-df_pyspark3 = spark.read.csv('test3.csv', header=True, inferSchema=True)
+# df_pyspark3 = spark.read.csv('test3.csv', header=True, inferSchema=True)
 
-# df_pyspark3.show()
+# # df_pyspark3.show()
 
